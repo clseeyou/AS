9364044609e3e747bbe7b17a0abdfdd11200956b
chenli <chenli@172-10-22-213.lightspeed.clmasc.sbcglobal.net>
2016-02-18 15:26:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chenli/as/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.chenli.as;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.chenli.mylibrary.UserTest;
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Picasso.with(this);
+        new Gson();
+        ImageLoader.getInstance();
+
+        new UserTest().getName();
+
+        new AsyncHttpClient();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ mylibrary/src/main/java/com/example/chenli/mylibrary/UserTest.java
@@ -0,0 +1,25 @@
+package com.example.chenli.mylibrary;
+
+/**
+ * Created by chenli on 16/2/18.
+ */
+public class UserTest {
+    private String name;
+    private String pwd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
